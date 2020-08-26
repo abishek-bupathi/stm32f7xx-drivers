bd949b5c79b18e509dbed52f685e9b72157b007e
Abishek Bupathi <abishekb128@gmail.com>
2020-08-26 10:00:25 +0100

    completed sample code for external button and Led

--- Debug/Src/03_led_button_ext.d
+++ Debug/Src/03_led_button_ext.d
@@ -1,4 +1,4 @@
-Src/02_led_button.o: ../Src/02_led_button.c \
+Src/03_led_button_ext.o: ../Src/03_led_button_ext.c \
  D:/CODING/STM32/STM32F7XX_DRIVERS/Drivers/Inc/stm32f767xx.h \
  D:/CODING/STM32/STM32F7XX_DRIVERS/Drivers/Inc/stm32f767xx_gpio_driver.h \
  D:/CODING/STM32/STM32F7XX_DRIVERS/Drivers/Inc/stm32f767xx.h
--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -4,16 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/02_led_button.c 
+../Src/03_led_button_ext.c 
 
 OBJS += \
-./Src/02_led_button.o 
+./Src/03_led_button_ext.o 
 
 C_DEPS += \
-./Src/02_led_button.d 
+./Src/03_led_button_ext.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/02_led_button.o: ../Src/02_led_button.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DSTM32 -DNUCLEO_F767ZI -DSTM32F7 -DDEBUG -DSTM32F767ZITx -c -I../Inc -I"D:/CODING/STM32/STM32F7XX_DRIVERS/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/02_led_button.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/03_led_button_ext.o: ../Src/03_led_button_ext.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DSTM32 -DNUCLEO_F767ZI -DSTM32F7 -DDEBUG -DSTM32F767ZITx -c -I../Inc -I"D:/CODING/STM32/STM32F7XX_DRIVERS/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/03_led_button_ext.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
 
